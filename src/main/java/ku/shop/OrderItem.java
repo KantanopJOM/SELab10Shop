02663936de2aafbc64c8bff9e8d65c56755c834d
Kantanop Supannakoon <kantanop.s@ku.th>
2023-10-19 19:58:23 +0700

    shop

---
+++ src/main/java/ku/shop/OrderItem.java
@@ -0,0 +1,19 @@
+package ku.shop;
+
+public class OrderItem {
+    private int quantity;
+    private Product prod;
+
+    public OrderItem(Product prod, int quantity) {
+        this.prod = prod;
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return prod.getPrice() * quantity;
+    }
+
+    public int getQuantity() {
+        return prod.getStock();
+    }
+}
---
+++ src/main/java/ku/shop/ProductCatalog.java
@@ -0,0 +1,23 @@
+package ku.shop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductCatalog {
+
+    private Order order;
+
+    private Map<String, Product> products;
+
+    public ProductCatalog() {
+        products = new HashMap<>();
+    }
+
+    public void addProduct(String name, double price, int stock) {
+        products.put(name, new Product(name, price, stock));
+    }
+
+    public Product getProduct(String name) {
+        return products.get(name);
+    }
+}
